5cfb81dbec6dcde909c71fb56f96e5f14609a3b1
MarkTaylorTsai <marktaylortsai@gmail.com>
2026-09-03 10:48:03 +0800

    Complete C# Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What's your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What's your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
